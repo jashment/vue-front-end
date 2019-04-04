0ffc9ea50b6ea49748e1915f02daeb53bde26a76
jashment <jordanashment@gmail.com>
2019-04-04 17:34:38 +0000

    Wrong way to v-for

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -2,14 +2,13 @@
     <v-app>
         <v-layout>
             <v-card>
-                <v-img></v-img>
-                <v-card-title>
-                    <div>
-                        <h2>Animal Name</h2>
-                        <p>Description</p>
-                    </div>
-                </v-card-title>
-
+                    <v-img></v-img>
+                    <v-card-title>
+                        <div>
+                            <h2>Animal Name</h2>
+                            <p>Description</p>
+                        </div>
+                    </v-card-title>
             </v-card>
         </v-layout>
     </v-app>

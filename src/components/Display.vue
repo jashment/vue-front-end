557ada38940f22e2bcf06aafb7e849bfa677ec14
Jordan <jordanashment@gmail.com>
2019-04-08 15:20:28 -0600

    creating and building components

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,22 +1,58 @@
 <template>
     <v-container>
-        <v-layout>
-            <v-card>
+        <v-layout row wrap>
+            <!--<v-card>
                 <v-img :src="`https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500`" />
                     <v-card-title>
-                        
-                            <h2>Animal Name</h2>
-                            <p>Description</p>
-                            <p>Other Description</p>
+                        <h2>Animal Name</h2>
+                        <p>Description</p>
+                        <p>Other Description</p>
                     </v-card-title>
-            </v-card>
+            </v-card>-->
+
+            <animalCard v-for="animal in animals"
+            :key="animal.name"
+            :animal="animal"
+            />
+
+            <v-flex
+              v-for="n in 9"
+              :key="n"
+              xs4
+            >
+
+            </v-flex>
         </v-layout>
     </v-container>
 </template>
 
 <script>
+import axios from 'axios'
+import gql from 'graphql-tag'
+import animalCard from './animalCard.vue'
+
     export default {
-        name: "Display"
+        name: "Display",
+        components: {
+            animalCard
+        },
+        data() {
+            return {
+                info: "",
+                animals: []
+            }
+        },
+        mounted() {
+            // 
+        },
+        apollo: {
+            animals: gql`query{
+                animals {
+                    name
+                    description
+                }
+            }`,
+        }
     }
 </script>
 
---
+++ src/components/animalCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-card flat tile>
+        
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        animal: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+</style>

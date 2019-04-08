a1dd1b87300f25a7419e0077eefd7af38f170582
Jordan <jordanashment@gmail.com>
2019-04-07 23:24:10 -0600

    Still playing with the components

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -2,11 +2,12 @@
     <v-container>
         <v-layout>
             <v-card>
+                <v-img :src="`https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500`" />
                     <v-card-title>
                         
                             <h2>Animal Name</h2>
                             <p>Description</p>
-                        
+                            <p>Other Description</p>
                     </v-card-title>
             </v-card>
         </v-layout>

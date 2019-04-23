f7488440bf2790fe04b61a908794a81965b1564b
Jordan <jordanashment@gmail.com>
2019-04-22 20:12:58 -0600

    Installing and setting up Vue Router

--- src/main.js
+++ src/main.js
@@ -2,10 +2,20 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import { createProvider } from './vue-apollo'
+import { routes } from './router'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+const router = new VueRouter ({
+  routes,
+  mode: 'history'
+})
+
 new Vue({
   apolloProvider: createProvider(),
+  router,
   render: h => h(App)
 }).$mount('#app')

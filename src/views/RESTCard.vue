234eb425236e0387bb2b36a3ff178be2366c625e
Jordan <jordanashment@gmail.com>
2019-04-22 20:42:55 -0600

    Creating REST components

---
+++ src/views/RESTCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            //
+        }
+    },
+    created () {
+        //
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
